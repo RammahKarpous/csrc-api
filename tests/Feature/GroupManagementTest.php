c8a519f8da292466ff4eb42b626917614e927e42
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-05-12 14:43:43 +0100

    Update GroupManagementTest.php

--- tests/Feature/GroupManagementTest.php
+++ tests/Feature/GroupManagementTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Group;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -18,18 +19,26 @@ class GroupManagementTest extends TestCase
 
     public function test_club_official_can_create_a_family_group()
     {
-        $this->withExceptionHandling();
+        $this->withoutExceptionHandling();
 
-        $response = $this->post('/api/groups/store', [
+        $admin = User::factory()->create();
+
+        $response = $this->actingAs($admin)->post('/api/groups/store', $this->data());
+
+        $response->assertStatus(201);
+        $this->assertCount(1, Group::all());
+    }
+
+    public function data()
+    {
+        return [
             'family_name' => $this->faker->lastName,
+            'slug' => 'Hello',
             'address_line' => $this->faker->address,
             'place' => $this->faker->city,
             'postcode' => $this->faker->postcode,
             'contact_number' => $this->faker->phoneNumber,
             'email' => $this->faker->safeEmail
-        ]);
-
-        $response->assertStatus(201);
-        $this->assertCount(1, Group::all());
+        ];
     }
 }
